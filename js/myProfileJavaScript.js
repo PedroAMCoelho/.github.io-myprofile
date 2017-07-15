68a9167f2b3a41d404c53c47cfb2d4ebe45711d3
PedroAMCoelho <pedrocoelhoprojetos@gmail.com>
2017-07-15 01:40:47 -0300

    javascript function closeMenuToSee
    it closes the menu to make the sections visible (after user's click on home, about skills or contact)

--- js/myProfileJavaScript.js
+++ js/myProfileJavaScript.js
@@ -3,6 +3,7 @@ window.onload = function(){
 	var btnmenu = document.getElementsByClassName("btn-menu");
 	var menu = document.getElementsByClassName("menu");
 	btnclose = document.getElementsByClassName("btn-close");
+	var a = document.getElementsByTagName("a");
 
 	btnmenu[0].addEventListener("click",
 		function mostrarMenu() {
@@ -28,5 +29,17 @@ window.onload = function(){
 			}
 		});
 
+	for(var i=2;i<=5; i++){
+	a[i].addEventListener("click", 
+		function closeMenuToSee(){
+			if(menu[0].style.display=="block") 
+			{  
+				menu[0].style.display = "none";  
+			}    
+			else {  
+				menu[0].style.display = "block";  
+			}
+					
+		})};
 
 }
